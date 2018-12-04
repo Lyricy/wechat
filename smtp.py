cd2548854e92a3f0b3b59d3304a74806964d6f80
Lyricy <44635695+Lyricy@users.noreply.github.com>
2018-12-04 08:15:50 +0800

    Update smtp.py

--- smtp.py
+++ smtp.py
@@ -4,11 +4,11 @@ from email.header import Header
 
 # 第三方 SMTP 服务
 mail_host = "smtp.qq.com"  # 设置服务器
-mail_user = "2290222832@qq.com"  # 用户名
-mail_pass = "htlilsfuqedcebfj"  # 口令
+mail_user = "xxxxxxxxx@qq.com"  # 用户名
+mail_pass = "xxxxxxxxxxx"  # 口令
 
-sender = '2290222832@qq.com'
-receivers = ['765639340@qq.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
+sender = 'xxxxxxxxx@qq.com'
+receivers = ['xxxxxxx@qq.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
 
 message = MIMEText('Python 邮件发送测试...', 'plain', 'utf-8')
 message['From'] = Header("菜鸟教程", 'utf-8')
